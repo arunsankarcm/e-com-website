ec6997f0d2c0990a386a36db8e6049ca4083e207
arunsankarcm <190276@tkmce.ac.in>
2023-12-04 12:39:19 +0530

    Delete src directory

--- src/Cart.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Cart = () => {
-    return (
-        <p>hello</p>
-    )
-}
-
-export default Cart;
--- src/Shop.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Shop = () => {
-    return(
-        <p>hello</p>
-    )
-}
-
-export default Shop;
